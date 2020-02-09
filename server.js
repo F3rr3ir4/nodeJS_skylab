4a86ce15ee8e0e318cd38f75d9d94f445a50fc1f
F3rr3ir4 <leandroferreira757@msn.com>
2020-02-09 17:54:37 -0300

    cleat server

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Ferreira Gatão");
-});
+mongoose.connect(
+  "mongodb+srv://ferreira:F3rr31r4321@cluster0-2qq0n.mongodb.net/nodeJsSkylab?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+requireDir("./src/models");
+
+app.use("/api", require("./src/routes"));
 app.listen(3001);
