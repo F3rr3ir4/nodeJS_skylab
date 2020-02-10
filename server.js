69ede810a0b258f3c140ed6c8712d884987e0c99
F3rr3ir4 <leandroferreira757@msn.com>
2020-02-09 23:42:24 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://ferreira:F3rr31r4321@cluster0-2qq0n.mongodb.net/nodeJsSkylab?retryWrites=true&w=majority",
